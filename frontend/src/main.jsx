c33b733ccaf36b7bfd9945bb46b61fbb475b5d82
Satvik-Pisipati <satvik.pisipati176@gmail.com>
2025-11-11 18:39:22 +0100

    backup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { WatchlistProvider } from "./state/WatchlistContext.jsx";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
