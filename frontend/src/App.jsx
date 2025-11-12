124d7b3833cf3c95c97131a01c7832275ff61f13
Pascal Hofstetter <pascal.hofstetter@ost.ch>
2025-11-12 19:40:54 +0100

    change to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import Dashboard from "./pages/Dashboard";
 import MainPage from "./pages/MainPage";
 import WatchlistPage from "./pages/WatchlistPage";
+import MyRatingsPage from "./pages/MyRatingsPage";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -20,6 +21,7 @@ function App() {
       <Route path="/dashboard" element={<Dashboard user={user} />} />
       <Route path="/home" element={<MainPage user={user} />} />
       <Route path="/watchlist" element={<WatchlistPage user={user} />} />
+      <Route path="/ratings" element={<MyRatingsPage user={user} />} />
     </Routes>
   );
 }
